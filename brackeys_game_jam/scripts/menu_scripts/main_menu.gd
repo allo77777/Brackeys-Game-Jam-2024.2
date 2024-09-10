10c59cd21ec68fd82e56e3720c92a05dde9818f6
allo77777 <ahmedsamaoui832@gmail.com>
2024-09-09 21:11:14 -0500

    functional menu
    the main menu is set as the main scene. Has a play button that switch to level scene

---
+++ brackeys_game_jam/scripts/menu_scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu_scenes/credits.tscn")
+	
+
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
